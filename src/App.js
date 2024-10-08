2626f6054b897e148f7732c62a741a42b2feb3ae
Gunavathi1224 <rgunawathi200870@gmail.com>
2024-10-08 14:53:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <center>
+        <h1>GUNAVATHI</h1>
+        <h3>WEB DEVELOPER</h3>
+        <hr></hr>
+
+        <h5>5/43 pasupathikovil thanjavur</h5>
+        <h5>rgunawathi200870@gmail.com</h5>
+        <h5>9344568988</h5>
+        <hr></hr>
+    </center>
+
+    <center> 
+      <h3>PROFILE</h3>
+      <P>
+      I am qualified and professional web developer.Strong creative and analytical skills.team player with an eye for details.Seeking for an entry level opportunities with an eseteemed organisation where i can utilise my skills and enhance learning in the field of work.
+      </P>
+      <hr></hr>
+    </center>
+    
+        <center>
+            <h3>TECHNICAL SKILLS</h3>
+       <ul>
+           <li> html</li>
+           <li>css</li>
+           <li>javascript</li>
+           <li>python</li>
+       </ul>
+       <hr></hr>
+       <h3>EDUCATION</h3>
+       <h4>Bachelor Of Engineering</h4>
+       <h4>Vetri Vinayaha College Of Engineering & Technology</h4>
+       <hr></hr>
+       <h3>SKILLS</h3>
+       <ul>
+           <li>leadership</li>
+           <li>Discipline</li>
+           <li>Self-confident</li>
+       </ul>
+       <hr></hr>
+       <h3>PROJECT</h3>
+       <h4>portfolio</h4>
+       <p>
+          The portfolio is developed using reactjs a popular javascript library for building user interface.Additionally technolgy include Html,css.
+          </p>
+          <hr></hr>
+          <h3>CERTIFICATION</h3>
+          <h4>Full stack development</h4>
+          <h5>Novitech I Coimbatore</h5>
+        
+        </center>
+
+    </div>
+  );
+}
+
+export default App;
